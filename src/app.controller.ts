4c48e68a88c4aa776c4072fce8560a293799ef70
HEEJUICE <kljuyt107@gmail.com>
2023-01-06 20:44:10 +0900

    :adhesive_bandage: Fix: Fix illogical name
    validate -> check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  validateSpam(): boolean {
+  checkSpam(): boolean {
     return this.appService.isSpam();
   }
 }
